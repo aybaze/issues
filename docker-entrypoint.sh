09e93315bb9bec74aadfe978525c3138660cbfed
Christian Banse <oxisto@aybaze.com>
2019-10-27 22:17:47 +0100

    Smaller fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,5 +14,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-./restore.sh $AYBAZE_POSTGRES
-./issues $AYBAZE_POSTGRES
+./restore.sh $ISSUES_POSTGRES
+./issues
